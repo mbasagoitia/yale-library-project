9c51d9c3db43a5f3fe9509cf9f1ffd736e1a82e2
Marika Basagoitia <marika.basagoitia@gmail.com>
2024-06-11 12:58:17 -0400

    added sqlite to project and configured server file

--- src/server/server.js
+++ src/server/server.js
@@ -1,57 +1,59 @@
-import express from "express";
-import session from 'express-session';
-import * as expressSession from "express-session";
-import expressMySqlSession from "express-mysql-session";
-const MySQLStore   = expressMySqlSession(expressSession);
-
-// Import routes
-
-import path from 'path';
-import { join } from "path";
-import { fileURLToPath } from 'url';
-import morgan from "morgan";
-import cors from "cors";
-import errorHandler from "./middlewares/errorHandler.js";
-import mysql from 'mysql2/promise';
+const express = require('express');
+// const session = require('express-session');
+// const MySQLStore = require('express-mysql-session')(session);
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+// const nodemon = require('nodemon');
+const path = require('path');
+const morgan = require('morgan');
+// const errorHandler = require('./middlewares/errorHandler.js');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+
+
+// app.use(nodemon);
 app.use(morgan("dev"));
 
+app.use(cors());
+app.use(morgan('dev'));
+
+// Serve static files from the "public" directory
 app.use(express.static("public"));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Serve static files from the "client/build" directory
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
+// Database setup
+const db = new sqlite3.Database('./composers.db');
 
-const pool = mysql.createPool(); 
+// const dbConfig = {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// };
 
-const sessionStore = new MySQLStore({}, pool);
+// const pool = mysql.createPool(); 
 
-app.use(
-  session({
-    secret: process.env.SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-  })
-);
+// const sessionStore = new MySQLStore({}, pool);
+
+// app.use(
+//   session({
+//     secret: process.env.SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: sessionStore,
+//   })
+// );
+
+db.serialize(() => {
+  db.run("CREATE TABLE user (id INT, name TEXT)");
+});
 
 // Use relevant routers
-app.use("/api", apiRouter);
+// app.use("/api", apiRouter);
 
 app.use((req, res, next) => {
   try {
@@ -61,7 +63,7 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const port = process.env.port || 5000;
 
