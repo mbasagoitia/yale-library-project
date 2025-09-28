3945314890cdde972bca3dd987d8d3d47239a6e4
mbasagoitia <marika.basagoitia@gmail.com>
2025-09-28 11:41:46 -0400

    commented out yale font import for demo build

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
   // Only bundle Yale fonts in internal build
-if (process.env.REACT_APP_APP_MODE === 'internal') {
-  require('./assets/styles/fonts/yale-fonts.css');
-  document.documentElement.classList.add('yale-fonts');
-}
+// if (process.env.REACT_APP_APP_MODE === 'internal') {
+//   require('./assets/styles/fonts/yale-fonts.css');
+//   document.documentElement.classList.add('yale-fonts');
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
